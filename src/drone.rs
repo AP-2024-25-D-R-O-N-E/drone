cc8be2cb4cbca7244be09c5b4cf1c00f52ef7e46
Mattia <marmattia26@gmail.com>
2024-12-01 16:07:29 +0100

    First draft

--- src/drone.rs
+++ src/drone.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use crossbeam::{
     channel::{Receiver, Sender},
@@ -16,7 +16,7 @@ pub struct MyDrone {
     packet_send: HashMap<NodeId, Sender<Packet>>, //All the sender to other nodes.
     packet_recv: Receiver<Packet>, //This drone receiver, that will be linked to a sender given to every other drone.
     pdr: u8,                       //Would keep it in % to occupy less space, but could be f32.
-    floods_tracker: HashMap<NodeId, u64>, //not in the specification yet but will likely be needed to handle the Network Discovery Protocol
+    floods_tracker: HashSet<(NodeId, u64)>, //not in the specification yet but will likely be needed to handle the Network Discovery Protocol
 }
 
 impl DroneTrait for MyDrone {
@@ -40,7 +40,7 @@ impl DroneTrait for MyDrone {
                             PacketType::Nack(_nack)=>self.forward_packet(packet),
                             PacketType::Ack(_ack)=>self.forward_packet(packet),
                             PacketType::MsgFragment(fragment)=>self.handle_MsgFragment(fragment.fragment_index, packet),
-                            PacketType::FloodRequest(flood_request) => self.forward_packet(packet),
+                            PacketType::FloodRequest(flood_request) => self.forward_flood_request(flood_request.clone(), packet),
                             PacketType::FloodResponse(flood_response) => self.forward_packet(packet),
                         }
 
@@ -74,7 +74,7 @@ impl MyDrone {
             packet_send: ps,
             packet_recv: pr,
             pdr: (pdr * 100.0) as u8,
-            floods_tracker: HashMap::new(),
+            floods_tracker: HashSet::new(),
         }
     }
 
@@ -111,7 +111,6 @@ impl MyDrone {
     fn handle_MsgFragment(&self, index: u64, mut packet: Packet) {
         use rand::Rng;
 
-        println!("PROVA LOCAL");
         let prob: u8 = rand::thread_rng().gen_range(0..100);
         if prob < self.pdr {
             //reversing the route up to this point
@@ -134,108 +133,70 @@ impl MyDrone {
         self.forward_packet(packet);
     }
 
-    // fn forward_discovery_query(&self, mut flood_request: FloodRequest, packet: Packet) {
-    //     // packet: Packet
-    //     // the packet.packet_type was matched for PacketType::Query
-    //     // query: Query
-    //
-    //     // push node into path_trace, this assures that every edge can be reconstructed by the initiator node
-    //     flood_request
-    //         .path_trace
-    //         .push((self.drone_id, NodeType::Drone));
-    //
-    //     if (flood_request.ttl == 0) {
-    //         //only send back a result if the flooding is not a broadcast
-    //         if !flood_request.broadcasting {
-    //             let query_result = QueryResult {
-    //                 path_trace: flood_request.path_trace.clone(),
-    //                 flood_id: flood_request.flood_id,
-    //             };
-    //
-    //             let return_routing: Vec<u64> = flood_request
-    //                 .path_trace
-    //                 .iter()
-    //                 .rev()
-    //                 .map(|(node_id, _node_type)| *node_id)
-    //                 .collect();
-    //
-    //             let packet = Packet {
-    //                 pack_type: PacketType::QueryResult(query_result),
-    //                 routing_header: SourceRoutingHeader {
-    //                     hops: return_routing.clone(),
-    //                 },
-    //                 session_id: 0, // it'll be whatever for now
-    //             };
-    //
-    //             let first_hop_channel = self.neighbors.get(&return_routing[0]).unwrap();
-    //             // unwrap since given the construction of the hops we're guaranteed to not have any errors
-    //
-    //             let _ = first_hop_channel.send(packet);
-    //         }
-    //     } else {
-    //         //check if the flood was already managed on this node
-    //
-    //         let floodVal = self
-    //             .floods_tracker
-    //             .get(&flood_request.initiator_id)
-    //             .or(Some(&0));
-    //
-    //         if let Some(&flood_id) = floodVal {
-    //             if flood_id < flood_request.flood_id {
-    //                 // this means that the query's flood id is a new flood, thus it needs to be processed
-    //
-    //                 flood_request.ttl -= 1;
-    //
-    //                 for (neighbor_id, neighbor_channel) in &self.neighbors {
-    //                     let mut route = packet.routing_header.hops.clone();
-    //
-    //                     route.push(*neighbor_id);
-    //
-    //                     let packet = Packet {
-    //                         pack_type: PacketType::Query(flood_request.clone()),
-    //                         routing_header: SourceRoutingHeader { hops: route },
-    //                         session_id: 0, // it'll be whatever for now
-    //                     };
-    //                 }
-    //             } else {
-    //                 //flood's already passed through this node, thus send back a QueryResult
-    //
-    //                 //only send back a result if the flooding is not a broadcast
-    //                 if !flood_request.broadcasting {
-    //                     let query_result = QueryResult {
-    //                         path_trace: flood_request.path_trace.clone(),
-    //                         flood_id: flood_request.flood_id,
-    //                     };
-    //
-    //                     let mut return_routing: Vec<u64> = flood_request
-    //                         .path_trace
-    //                         .iter()
-    //                         .map_while(|(node_id, _node_type)| {
-    //                             if *node_id != self.id {
-    //                                 Some(*node_id)
-    //                             } else {
-    //                                 None
-    //                             }
-    //                         })
-    //                         .collect();
-    //
-    //                     return_routing.reverse();
-    //
-    //                     let packet = Packet {
-    //                         pack_type: PacketType::QueryResult(query_result),
-    //                         routing_header: SourceRoutingHeader {
-    //                             hops: return_routing.clone(),
-    //                         },
-    //                         session_id: 0, // it'll be whatever for now
-    //                     };
-    //
-    //                     let first_hop_channel = self.neighbors.get(&return_routing[0]).unwrap();
-    //                     // unwrap since given the construction of the hops we're guaranteed to not have any errors
-    //
-    //                     let _ = first_hop_channel.send(packet);
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
+    fn forward_flood_request(&mut self, mut flood_request: FloodRequest, packet: Packet) {
+        // packet: Packet
+        // the packet.packet_type was matched for PacketType::Query
+        // query: Query
+
+        // push node into path_trace, this assures that every edge can be reconstructed by the initiator node
+        flood_request
+            .path_trace
+            .push((self.drone_id, NodeType::Drone));
+
+        if self
+            .floods_tracker
+            .contains(&(flood_request.initiator_id, flood_request.flood_id))
+        {
+            //send flood response back, since node is already visited
+            let new_flood_res = FloodResponse {
+                path_trace: flood_request.path_trace.clone(),
+                flood_id: flood_request.flood_id,
+            };
+
+            let mut inverse_route = packet.routing_header.hops.clone();
+            inverse_route.truncate(packet.routing_header.hop_index + 1);
+            inverse_route.reverse();
+
+            let packet = Packet {
+                pack_type: PacketType::FloodResponse(new_flood_res),
+                routing_header: SourceRoutingHeader {
+                    hops: inverse_route,
+                    hop_index: 0,
+                },
+                session_id: 0, // it'll be whatever for now
+            };
+
+            self.forward_packet(packet);
+        } else {
+            // mark as visited and forward
+
+            println!("Discovered drone {}", self.drone_id);
+            self.floods_tracker
+                .insert((flood_request.initiator_id, flood_request.flood_id));
+
+            for (neighbor_id, neighbor_channel) in &self.packet_send {
+                let prev_hop_index = packet.routing_header.hop_index - 1;
+                if *neighbor_id != packet.routing_header.hops[prev_hop_index] {
+                    // do not send to the node the request is coming from
+                    let mut route = packet.routing_header.hops.clone();
+                    route.push(*neighbor_id);
+
+                    let mut new_flood_req = flood_request.clone();
+                    new_flood_req
+                        .path_trace
+                        .push((*neighbor_id, NodeType::Drone));
+
+                    let packet = Packet {
+                        pack_type: PacketType::FloodRequest(new_flood_req),
+                        routing_header: SourceRoutingHeader {
+                            hops: route,
+                            hop_index: packet.routing_header.hop_index,
+                        },
+                        session_id: 0, // it'll be whatever for now
+                    };
+                    self.forward_packet(packet);
+                }
+            }
+        }
+    }
 }
